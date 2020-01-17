af808a6b28e73ef17c529bec4be0d01665693250
Sabrina Markon <phpsitescripts@outlook.com>
2020-01-17 09:31:08 -0700

    Add OriginCheck to Ratchet chat server to make sure clients only connect from allowed domains.

--- chatServer/chat-server.php
+++ chatServer/chat-server.php
@@ -1,10 +1,16 @@
 <?php
 use Ratchet\Server\IoServer;
+use Ratchet\Http\OriginCheck;
 use Ratchet\Http\HttpServer;
 use Ratchet\WebSocket\WsServer;
 use chatClient\Chat;
 
     require dirname(__DIR__) . '/vendor/autoload.php';
+
+    # Make sure the client is connected from one of the allowed domains only.
+    $checkedApp = new OriginCheck(new WsServer(new Chat()), array('localhost'));
+    $checkedApp->allowedOrigins[] = 'phpsitescripts.com';
+    $checkedApp->allowedOrigins[] = 'collectorsscave.com';
 
     $server = IoServer::factory(
         new HttpServer(
