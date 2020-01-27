d59f59020836191f78c2326b919427be19dd6910
Sabrina Markon <phpsitescripts@outlook.com>
2020-01-26 17:18:19 -0700

    Remove unnecessary datetimepicker JS script.

--- admin/footer.php
+++ admin/footer.php
@@ -6,32 +6,8 @@
 
 </div><!-- main container -->
 
-
 <script src="//code.jquery.com/jquery.js"></script>
 <script src="/../js/bootstrap.min.js"></script>
 <script src="/../js/navigation.js"></script>
-<script type="text/javascript" src="/../js/datetimepicker/bootstrap-datetimepicker.js" charset="UTF-8"></script>
-<script type="text/javascript">
-    $('.form_date').datetimepicker({
-        language:  'en',
-        weekStart: 1,
-        todayBtn:  1,
-        autoclose: 1,
-        todayHighlight: 1,
-        startView: 2,
-        minView: 2,
-        forceParse: 0
-    });
-    $('.form_date_existing_edit').datetimepicker({
-        language:  'en',
-        weekStart: 1,
-        todayBtn:  1,
-        autoclose: 1,
-        todayHighlight: 1,
-        startView: 2,
-        minView: 2,
-        forceParse: 0
-    });
-</script>
 </body>
 </html>
