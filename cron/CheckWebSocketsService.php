b3f1a558f18c523b2fa422a5886ac89dd9b1fd1b
Sabrina Markon <phpsitescripts@outlook.com>
2020-01-26 17:09:57 -0700

    Create CheckWebSocketsService class for cron to check that websocket daemon is running.

---
+++ cron/CheckWebSocketsService.php
@@ -0,0 +1,29 @@
+<?php
+/*
+ * CheckWebSocketsService checks to make sure the websockets daemon is running, in case it failed.
+ *
+ * @author Sabrina Markon
+ * @copyright 2020 Sabrina Markon, PHPSiteScripts.com
+ * @license LICENSE.md
+ * 
+ */
+require_once('.../config/Database.php');
+require_once('.../config/Settings.php');
+require_once('.../classes/Email.php');
+
+class CheckWebSocketsService {
+
+  public function checkDaemon() {
+    
+  }
+}
+
+$sitesettings = new Settings();
+$settings = $sitesettings->getSettings();
+foreach ($settings as $key => $value)
+{
+    $$key = $value;
+}
+$checkwebsockets = new CheckWebSocketsService();
+$checkwebsockets->checkDaemon();
+
