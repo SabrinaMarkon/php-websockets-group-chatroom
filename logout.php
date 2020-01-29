f9d5a3b7afff944ee108f76755a61ff7ad24efb3
Sabrina Markon <phpsitescripts@outlook.com>
2020-01-28 17:29:09 -0700

    Rollback changes to logout.php. It still showed the members area navigation with the changes.

--- logout.php
+++ logout.php
@@ -1,7 +1,5 @@
 <?php
-$logout = new User();
-$logout->updateChatLoginStatus($_SESSION['username'], 0);
-$logout->userLogout();
-$showcontent = new PageContent();
-echo $showcontent->showPage('logout');
+if (isset($showlogout)) {
+  echo $showlogout;
+}
 ?>
